9d0a0a00fff2d06b4013d1e320e538032b9351e9
Your Name <your-email@example.com>
2025-09-01 17:15:54 +0530

    2nd Commit

--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">🚀 Tailwind is Working!</h1>
-    </div>
-  )
-}
+// app/page.js
+import { redirect } from "next/navigation";
 
+export default function Home() {
+  redirect("/login");
+}
